c73b120926dfe9358184bae865a68a8bdb88355d
heehunjung <114041681+heehunjung@users.noreply.github.com>
2024-07-30 12:16:03 +0000

    [꼭 다시 공부]

--- python/백준/bfs,dfs,sorting/1516.py
+++ python/백준/bfs,dfs,sorting/1516.py
@@ -1,2 +1,34 @@
 import sys
 from collections import deque
+
+N = int(input())
+graph = [[] for _ in range(N + 1)]
+indegree = [0] * (N + 1)
+cost = [0] * (N + 1)
+DP = [0] * (N + 1)
+
+for i in range(1, N + 1):
+  tmp = list(map(int, input().split()))[:-1]
+  cost[i] = tmp[0]
+  for j in tmp[1:]:
+    graph[j].append(i)
+    indegree[i] += 1
+
+q = deque()
+
+for i in range(1, N + 1):
+  if not indegree[i]:
+    q.append(i)
+    DP[i] = cost[i]
+
+while q:
+  n = q.popleft()
+
+  for i in graph[n]:
+    indegree[i] -= 1
+    DP[i] = max(DP[i], DP[n] + cost[i])
+
+    if not indegree[i]:
+      q.append(i)
+for i in range(1, N + 1):
+  print(DP[i], end='\n')
