490c679ac5166a721ae0e590991d01be6201c367
heehunjung <114041681+heehunjung@users.noreply.github.com>
2024-08-09 06:55:15 +0000

    [dp] fibo

--- python/dynamicPrograming/1.py
+++ python/dynamicPrograming/1.py
@@ -1,6 +1,13 @@
-def fibo(x):
-  if x==1 or x==2:
-    return 1
-  return fibo(x-1)+fibo(x-2)
+dp = [0] * 100
 
-print((fibo(4)))
+
+def fibo(x):
+  if x == 1 or x == 2:
+    return 1
+  if dp[x] != 0:
+    return dp[x]
+  dp[x] = fibo(x - 1) + fibo(x - 2)
+  return dp[x]
+
+
+print((fibo(99)))
