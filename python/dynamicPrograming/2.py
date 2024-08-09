8f8b7f457e5c0e5c19fdbf6da23e960396d13430
heehunjung <114041681+heehunjung@users.noreply.github.com>
2024-08-09 07:32:33 +0000

    [dp] fix

--- python/dynamicPrograming/2.py
+++ python/dynamicPrograming/2.py
@@ -8,6 +8,6 @@ dp[0] = list[0]
 dp[1] = list[1]
 dp[2] = list[0] + list[2]
 for i in range(3, n):
-  dp[i] = max(dp[i - 2] + list[i], dp[i - 3] + list[i])
+  dp[i] = max(dp[i - 2] + list[i], dp[i - 1])
 
 print(dp[n - 1])
