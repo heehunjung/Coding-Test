faa029ab38c0ce7b0395500d9d6634d955cbd9ee
heehun <slrkqk2@naver.com>
2024-08-19 13:59:45 +0900

    [dp} 11722

---
+++ python/백준/dp/11722.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+array = list(map(int,input().split()))
+dp = [0] * len(array)
+dp[0] = 1
+for i in range(1,len(array)):
+    for j in range(0,i):
+        if array[i]< array[j]:
+            dp[i] = max(dp[i],dp[j]+1)
+    if dp[i] == 0:
+        dp[i] = 1
+
+result = max(dp)
+
+print(result)
