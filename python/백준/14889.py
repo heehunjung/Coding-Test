0dd1d325e5cb47f15bd81597b943046d5e25ac90
heehun <slrkqk2@naver.com>
2024-09-08 15:45:31 +0900

    14889.py - 조합 중복 줄이기

--- python/백준/14889.py
+++ python/백준/14889.py
@@ -4,7 +4,7 @@ import sys
 input = sys.stdin.readline
 N = int(input())
 
-people = [i+1 for i in range(N)]
+people = [i for i in range(2,N+1)]
 power = [[0 for _ in range(N+1)] for _ in range(N+1)]
 
 
@@ -14,19 +14,22 @@ for i in range(1,N+1):
         power[i][j] = row[j-1]
 
 result = -1
+fixed_people = 1
 
-for teamA in combinations(people,N//2):
+for teamA in combinations(people,(N//2-1)):
     teamA_result = 0
     teamB_result = 0
     temp_result = 0
-    for memberA in teamA:
-        for memberB in teamA:
+    teamA_origin = teamA+(fixed_people,)
+    for memberA in teamA_origin:
+        for memberB in teamA_origin:
             if memberA != memberB:
                 teamA_result += power[memberA][memberB]
                 
     teamB = list(set(people)-set(teamA))
     
-    print("teamA:",teamA,"teamB:",teamB )
+    # print("teamA:",teamA,"teamB:",teamB)
+    
     for memberA in teamB:
         for memberB in teamB:
             if memberA != memberB:
