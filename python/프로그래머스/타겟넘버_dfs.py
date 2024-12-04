fa42ee319a1fd19feb1b0604086dd8bb82b3d83b
heehun <slrkqk2@naver.com>
2024-12-04 09:50:18 +0900

    스코프

--- python/프로그래머스/타겟넘버_dfs.py
+++ python/프로그래머스/타겟넘버_dfs.py
@@ -4,17 +4,17 @@ def solution(numbers, target):
     for number in numbers:
         stack.append([number,number * -1])
     
-    def dfs(stack, numbers,target ,index, current):
+    def dfs(stack ,index, current):
         nonlocal answer
         
         if index < len(stack):
             for number in stack[index]:
-                dfs(stack,numbers,target ,index + 1 ,current + number)
+                dfs(stack ,index + 1 ,current + number)
         else:
             if current == target:
                 answer += 1
     
-    dfs(stack,numbers,target,0,0)
+    dfs(stack,0,0)
             
      
     return answer
